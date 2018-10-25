b9d1e2d99800e9faa3910b609708608da5b5d3ea
liyong561 <2869486287@qq.com>
2018-10-25 10:50:19 +0800

    10/25new

---
+++ newswebsite/src/main/java/com/example/newswebsite/NewswebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.example.newswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewswebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewswebsiteApplication.class, args);
+	}
+}
