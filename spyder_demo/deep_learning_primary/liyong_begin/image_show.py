7abcc7a03756fc80a8af1593659c16f252ba1e92
liyong561 <2869486287@qq.com>
2018-11-07 11:45:05 +0800

    research

---
+++ spyder_demo/deep_learning_primary/liyong_begin/image_show.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+img=imread('E:/Data/Images/Person/black_girl.jpg')
+plt.imshow(img) # tell the class method how to plot the picture
+plt.show()
+
